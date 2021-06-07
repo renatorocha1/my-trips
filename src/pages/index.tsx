a58dbecd439ee5ce7bcfd4dc9d07cb7c05315707
Renato Rocha <engirocha@gmail.com>
2021-06-08 05:15:35 +1200

    add prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ type Props = {
   title: string
 }
 
-export default function Home({ title = 'Hello World' } : Props) {
+export default function Home({ title = 'Hello World' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
