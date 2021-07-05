3c7487f0809c0c9c55c7f780a5567371d1249f42
Renato Rocha de Almeida <rocha@Renatos-MBP.hub>
2021-07-06 09:49:45 +1200

    add test to map component and textwrapper

--- src/components/TextWrapper/test.tsx
+++ src/components/TextWrapper/test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import LinkWrapper from '.'
 
 describe('<LinkWrapper/>', () => {
-  it('should render link and chindrel', () => {
+  it('should render link and children', () => {
     render(<LinkWrapper href="/my-link">Anything</LinkWrapper>)
     const children = screen.getByText(/anything/i)
     expect(children).toBeInTheDocument()
